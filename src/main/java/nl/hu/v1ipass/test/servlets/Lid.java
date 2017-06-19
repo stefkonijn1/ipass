564338f979f89c32fe7c50aff46df112cbba3e9d
Thuis <stefman99@gmail.com>
2017-06-19 22:17:06 +0200

    no message

--- src/main/java/nl/hu/v1ipass/test/servlets/Lid.java
+++ src/main/java/nl/hu/v1ipass/test/servlets/Lid.java
@@ -50,8 +50,8 @@ public class Lid extends HttpServlet {
 	 out.println(" <body>");
 	 out.println(" <p><img src='vep1.jpg' alt='vep1' width='700' height='100'></p>");
 	 out.println(" <ul>");
-	 out.println(" <li><a class='active' href='http://localhost:1555/test/home_lid.html'>Home</a></li>");
-	 out.println("  <li><a href='http://localhost:1555/test/stand_lid.html'>Stand</a></li>");
+	 out.println(" <li><a class='active' href='home_lid.html'>Home</a></li>");
+	 out.println("  <li><a href='stand_lid.html'>Stand</a></li>");
 	 out.println("  <li><a href='http://localhost:1555/test/prog_lid.html'>Programmma</a></li>");
 
 	 out.println("</ul>");
